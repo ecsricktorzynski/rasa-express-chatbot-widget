ba78175c92674eb6c56a804f5e65b54c95c4b2f0
Rick Torzynski <richard.torzynski@associates.hq.dhs.gov>
2022-09-11 14:31:54 -0400

    Add existing files to repo

---
+++ app/public/js/lib/index.js
@@ -0,0 +1,15 @@
+
+/* module for importing other js files */
+function include(file) {
+    const script = document.createElement('script');
+    script.src = file;
+    script.type = 'text/javascript';
+    script.defer = true;
+
+    document.getElementsByTagName('head').item(0).appendChild(script);
+}
+
+include('./js/lib/jquery.min.js');
+include('./js/lib/materialize.min.js');
+include('./js/lib/chart.min.js');
+include('./js/lib/uuid.min.js');
